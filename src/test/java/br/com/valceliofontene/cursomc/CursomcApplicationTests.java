c3c4c89124132ff8460337a0c070b9a0f85bda92
Valcelio Fontenle <vfonte2000@gmail.com>
2021-05-29 01:03:07 -0300

    projeto criado

---
+++ src/test/java/br/com/valceliofontene/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.valceliofontene.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
