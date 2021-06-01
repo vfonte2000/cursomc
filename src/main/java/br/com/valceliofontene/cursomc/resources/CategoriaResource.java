0d008c1fc9797211e082aabc19a47e789c92c3d1
Valcelio Fontenle <vfonte2000@gmail.com>
2021-06-01 00:12:29 -0300

    Testando a prieira classe de dominio - Categoria

--- src/main/java/br/com/valceliofontene/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/valceliofontene/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,31 @@
 package br.com.valceliofontene.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.valceliofontene.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST esta funcionando!";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		Categoria cat3 = new Categoria(3, "vfonteCompany");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		lista.add(cat3);
+		
+		return lista;
 	}
 
 }
